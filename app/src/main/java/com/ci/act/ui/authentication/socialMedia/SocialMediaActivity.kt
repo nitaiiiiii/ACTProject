be4c86bc12ae2d3ebeec8dbad35779bc7bdd7ebf
Gowtham <hadaipadmavathi@gmail.com>
2022-11-01 17:34:39 +0530

    second commit

--- app/src/main/java/com/ci/act/ui/authentication/socialMedia/SocialMediaActivity.kt
+++ app/src/main/java/com/ci/act/ui/authentication/socialMedia/SocialMediaActivity.kt
@@ -2,6 +2,7 @@ package com.ci.act.ui.authentication.socialMedia
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import com.ci.act.BR
@@ -26,20 +27,9 @@ class SocialMediaActivity :
     override fun getBindingVariable(): Int = BR.socialMedia
 
     override fun initViews(savedInstanceState: Bundle?) {
-        spannableStrings()
+        spannableText()
         setOnClickListener()
     }
-
-    private fun spannableStrings() {
-        val text = "Sign Up"
-
-        val spannableString = SpannableString(text).apply {
-            setSpan(UnderlineSpan(), 0, text.length, 0)
-        }
-        mViewDataBinding?.txtSocialMediaEmailSignUpScreen?.text = spannableString
-
-    }
-
     private fun setOnClickListener() {
         mViewDataBinding?.txtSocialMediaEmailLoginScreen?.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
@@ -51,6 +41,31 @@ class SocialMediaActivity :
             startActivity(action)
             finish()
         }
+
+    }
+
+    private fun spannableText() {
+
+        //these for Terms
+        val spannableString = SpannableString("By Signing in, you agree to our Terms & Privacy")
+        val underlineSpan = UnderlineSpan()
+        spannableString.setSpan(underlineSpan, 32, 37, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        mViewDataBinding?.txtSocialMediaScreen?.setText(spannableString)
+
+
+        //these for Privacy for
+        val underlineSpan2 = UnderlineSpan()
+        spannableString.setSpan(underlineSpan2, 40, 47, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        mViewDataBinding?.txtSocialMediaScreen?.setText(spannableString)
+
+
+        //for signUp
+        //txtSocialMediaEmailSignUpScreen
+        val signUp = SpannableString("Sign Up")
+        val underlineSpan3 = UnderlineSpan()
+        signUp.setSpan(underlineSpan3, 0, 6, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        mViewDataBinding?.txtSocialMediaEmailSignUpScreen?.setText(signUp)
+
 
     }
 
