24922f8700e18ac6ad26770bca0e4ba3dcdc92d0
Gowtham <hadaipadmavathi@gmail.com>
2022-11-09 17:30:38 +0530

    commit from act.

--- app/src/main/java/com/ci/act/ui/home/mainEventScreenRegister/fragments/guardianApproval/GuardianApprovalViewModel.kt
+++ app/src/main/java/com/ci/act/ui/home/mainEventScreenRegister/fragments/guardianApproval/GuardianApprovalViewModel.kt
@@ -1,6 +1,20 @@
 package com.ci.act.ui.home.mainEventScreenRegister.fragments.guardianApproval
 
+import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ci.act.base.BaseViewModel
+import com.ci.act.ui.home.mainEventScreenRegister.fragments.guardianApproval.page.SignatureModel
 
 class GuardianApprovalViewModel : BaseViewModel<GuardianApprovalView>() {
+
+    private val signature : MutableLiveData<SignatureModel> = MutableLiveData()
+
+    fun setSignature(isFrom : String, bitmap : Bitmap) {
+        signature.value = SignatureModel(isFrom, bitmap)
+    }
+
+    fun getSignature() : LiveData<SignatureModel> {
+        return signature
+    }
 }
---
+++ app/src/main/java/com/ci/act/ui/home/mainEventScreenRegister/fragments/guardianApproval/page/SignatureModel.kt
@@ -0,0 +1,8 @@
+package com.ci.act.ui.home.mainEventScreenRegister.fragments.guardianApproval.page
+
+import android.graphics.Bitmap
+
+class SignatureModel(
+    val isFrom : String,
+    val bitmap : Bitmap
+)
