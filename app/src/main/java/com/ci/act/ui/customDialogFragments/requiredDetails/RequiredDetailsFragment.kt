1ffa9d2dc2861d1e1d4999342d38063d7b47ea86
Gowtham <hadaipadmavathi@gmail.com>
2022-11-15 19:01:45 +0530

    commit from act.

--- app/src/main/java/com/ci/act/ui/customDialogFragments/requiredDetails/RequiredDetailsFragment.kt
+++ app/src/main/java/com/ci/act/ui/customDialogFragments/requiredDetails/RequiredDetailsFragment.kt
@@ -1,7 +1,9 @@
 package com.ci.act.ui.customDialogFragments.requiredDetails
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import androidx.navigation.Navigation
 import com.ci.act.BR
 import com.ci.act.R
 import com.ci.act.base.BaseDialogFragmentNew
@@ -9,10 +11,18 @@ import com.ci.act.databinding.FragmentRequiredDetailsBinding
 import com.ci.act.ui.authentication.forgotPassword.ForgotPasswordActivity
 import com.ci.act.ui.authentication.signin.SignInActivity
 import com.ci.act.ui.authentication.signup.SignUpActivity
+import com.ci.act.ui.authentication.socialMedia.SocialMediaActivity
+import com.ci.act.util.showSnackBar
+import java.util.*
 
 class RequiredDetailsFragment :
     BaseDialogFragmentNew<FragmentRequiredDetailsBinding, RequiredDetailsView, RequiredDetailsViewModel>(),
     RequiredDetailsView {
+
+    var calendarDay = 0
+    var calendarMonth = 0
+    var calendarYear = 0
+
     override fun getContentView(): Int = R.layout.fragment_required_details
 
     override fun setViewModelClass(): Class<RequiredDetailsViewModel> {
@@ -40,7 +50,64 @@ class RequiredDetailsFragment :
             val intent = Intent(requireActivity(),SignUpActivity::class.java)
             startActivity(intent)
         }
+        mViewDataBinding?.btnRequiredLocation?.setOnClickListener {
+            if (mViewDataBinding?.editTextRequiredDetailsEmail?.text?.isEmpty()!!) {
+                showSnackBar(requireView(), "Email Address is required...", false)
+                mViewDataBinding?.editTextRequiredDetailsEmail?.requestFocus()
+            } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(
+                    mViewDataBinding?.editTextRequiredDetailsEmail?.text?.trim().toString()
+                ).matches()
+            ) {
+                showSnackBar(requireView(), "Not a valid email address...", false)
+                mViewDataBinding?.editTextRequiredDetailsEmail?.requestFocus()
+            } else if (mViewDataBinding?.editTextRequiredDetailsDateOfBirth?.text?.isEmpty()!!) {
+                showSnackBar(requireView(), "Date of Birth is required...", false)
+            } else {
+                dialog?.dismiss()
+                ForgotPasswordActivity.isRequiredDetails = false
+                val intent = Intent(requireActivity(),SocialMediaActivity::class.java)
+                startActivity(intent)
+
+
+
+            }
+        }
+
+        mViewDataBinding?.editTextRequiredDetailsDateOfBirth?.setOnClickListener {
+            getDateTimeCalender()
+        }
     }
+
+    private fun getDateTimeCalender() {
+        val calendar = Calendar.getInstance()
+        calendarDay = calendar.get(Calendar.DAY_OF_MONTH)
+        calendarMonth = calendar.get(Calendar.MONTH)
+        calendarYear = calendar.get(Calendar.YEAR)
+
+        val datePickerDialog = DatePickerDialog(requireContext(), R.style.DatePickerTheme_Dark, DatePickerDialog.OnDateSetListener { view, year, month, day ->
+            val month = month + 1
+            var sMonth = ""
+            var sDay = ""
+            sMonth = if (month < 10) {
+                "0$month"
+            } else {
+                month.toString()
+            }
+            sDay = if (day < 10) {
+                "0$day"
+            } else {
+                day.toString()
+            }
+            val date = "$sMonth/$sDay/$year"
+            mViewDataBinding?.editTextRequiredDetailsDateOfBirth?.setText(date)
+        }, calendarYear, calendarMonth, calendarDay).let{
+            it.show()
+            it.setTitle("Date of Birth")
+            it.setMessage("Date of Birth")
+            it.datePicker.maxDate = System.currentTimeMillis()
+        }
+    }
+
 
     override fun addObservables() {
     }
